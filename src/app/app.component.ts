9e10c160e1e19a8292cfc6f5d59fe66e7dfea29b
Michał Kowalczyk <mickowalczyk6@gmail.com>
2025-01-14 18:53:47 +0100

    test commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'recipe-app-frontend v1';
+  title = 'New app with Angular';
 }
